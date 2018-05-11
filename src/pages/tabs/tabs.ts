dc13b26b28e5b3089fac5aa7ac80c19dced6fb94
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-11 21:59:19 +0200

    TO_DO list in ionic app

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { TodoComponent } from '../todo/todo.component';
+import { DoneComponent } from '../done/done.component';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = TodoComponent;
+  tab2Root = DoneComponent;
 
   constructor() {
 
