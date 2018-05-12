b34d04dab56496f8fb5c1ef1e580200815e5a6fa
LuisCabrero <100330021@alumnos.uc3m.es>
2018-05-12 11:45:43 +0200

    Interface design

--- src/pages/add/add.component.ts
+++ src/pages/add/add.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from 'ionic-angular';
+
 import { List, ListItem } from '../../app/classes/index';
+
+import { WishListService } from '../../app/services/wish-list.service';
 
 @Component({
   selector: 'app-add',
@@ -7,13 +11,17 @@ import { List, ListItem } from '../../app/classes/index';
 })
 export class AddComponent implements OnInit {
 
-  listName:string;
+  listName:string = "";
   itemName:string = "";
 
   items:ListItem[] = [];
 
 
-  constructor() {  }
+  constructor(
+    public alertCtrl:AlertController,
+    public _wishList:WishListService,
+    public navCtrl:NavController
+  ) {  }
 
   ngOnInit() {}
 
@@ -32,5 +40,25 @@ export class AddComponent implements OnInit {
 
   delete(i:number){
     this.items.splice(i,1);
+  }
+
+  saveList(){
+    if(this.listName.length == 0){
+      let alert = this.alertCtrl.create({
+        title: 'List name is required',
+        subTitle: 'You must specify a name for your list',
+        buttons: ['OK']
+      });
+      alert.present();
+      return;
+    }
+
+    let list = new List(this.listName);
+    list.items = this.items;
+    this._wishList.lists.push(list);
+
+    this.navCtrl.pop();
+
+
   }
 }
